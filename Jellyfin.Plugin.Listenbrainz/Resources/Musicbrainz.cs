455a8e495b7eb383a155b625daa54e2871f8d2f0
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2021-04-12 19:50:22 +0200

    Add Musicbrainz resources

---
+++ Jellyfin.Plugin.Listenbrainz/Resources/Musicbrainz.cs
@@ -0,0 +1,12 @@
+namespace Jellyfin.Plugin.Listenbrainz.Resources
+{
+    public static class Musicbrainz
+    {
+        public static string BaseUrl = "musicbrainz.org";
+
+        public static class Endpoints
+        {
+            public static string Recording = "recording";
+        }
+    }
+}
