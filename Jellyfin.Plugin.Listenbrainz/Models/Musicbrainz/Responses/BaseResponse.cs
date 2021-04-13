73f3a89a1c2d3e450e62f879d9e4d7afa01a8db0
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2021-04-13 08:45:50 +0200

    Add error property

--- Jellyfin.Plugin.Listenbrainz/Models/Musicbrainz/Responses/BaseResponse.cs
+++ Jellyfin.Plugin.Listenbrainz/Models/Musicbrainz/Responses/BaseResponse.cs
@@ -1,14 +1,23 @@
+using System.Runtime.Serialization;
 using System.Xml.Linq;
 
 namespace Jellyfin.Plugin.Listenbrainz.Models.Musicbrainz.Responses
 {
     public class BaseResponse
     {
+        [DataMember(Name = "error")]
+        public string Error { get; set; }
+
         public XElement XmlData { get; set; }
 
         public BaseResponse(XElement xmlData)
         {
             XmlData = xmlData;
+        }
+
+        public BaseResponse(string error)
+        {
+            Error = error;
         }
 
         public virtual string GetData() => "";
