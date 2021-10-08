e3fb2818b58cf0358edc41477f755631d0bd02db
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2021-10-09 00:22:38 +0200

    Fix deserialization of validate token response

--- Jellyfin.Plugin.Listenbrainz/Models/Listenbrainz/Responses/ValidateTokenResponse.cs
+++ Jellyfin.Plugin.Listenbrainz/Models/Listenbrainz/Responses/ValidateTokenResponse.cs
@@ -1,13 +1,13 @@
-using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace Jellyfin.Plugin.Listenbrainz.Models.Listenbrainz.Responses
 {
     public class ValidateTokenResponse : BaseResponse
     {
-        [DataMember(Name = "user_name")]
+        [JsonPropertyName("user_name")]
         public string Name { get; set; }
 
-        [DataMember(Name = "valid")]
+        [JsonPropertyName("valid")]
         public bool Valid { get; set; }
 
         public override bool IsError() => !Valid || Code == 400;
