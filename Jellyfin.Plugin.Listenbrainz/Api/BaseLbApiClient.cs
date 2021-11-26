c65a7ed2e800309877990b8b565fe2762c71d840
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2021-11-27 00:40:18 +0100

    Fix docstring

--- Jellyfin.Plugin.Listenbrainz/Api/BaseLbApiClient.cs
+++ Jellyfin.Plugin.Listenbrainz/Api/BaseLbApiClient.cs
@@ -30,7 +30,7 @@ namespace Jellyfin.Plugin.Listenbrainz.Api
         }
 
         /// <summary>
-        /// Send a POST request to the Listenbrainz Api
+        /// Send a POST request to the Listenbrainz server
         /// </summary>
         /// <typeparam name="TRequest">The type of the request</typeparam>
         /// <typeparam name="TResponse">The type of the response</typeparam>
