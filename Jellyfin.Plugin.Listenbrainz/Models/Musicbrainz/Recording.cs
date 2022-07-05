1fcbd8fff06b2d58610e951e7cebe0ed8d440639
Dominik Krivohlavek <github@xkrivo.net>
2022-07-05 14:19:50 +0200

    Get artist credit for recording

--- Jellyfin.Plugin.Listenbrainz/Models/Musicbrainz/Recording.cs
+++ Jellyfin.Plugin.Listenbrainz/Models/Musicbrainz/Recording.cs
@@ -1,3 +1,7 @@
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Text.Json.Serialization;
+
 namespace Jellyfin.Plugin.Listenbrainz.Models.Musicbrainz;
 
 /// <summary>
@@ -11,6 +15,7 @@ public class Recording
     public Recording()
     {
         Id = string.Empty;
+        ArtistCredit = new Collection<ArtistCredit>();
     }
 
     /// <summary>
@@ -22,4 +27,52 @@ public class Recording
     /// Gets or sets recording match score.
     /// </summary>
     public int Score { get; set; }
+
+    /// <summary>
+    /// Gets or sets artist credit of the recording.
+    /// </summary>
+    [JsonPropertyName("artist-credit")]
+    public Collection<ArtistCredit> ArtistCredit { get; set; }
+
+    /// <summary>
+    /// Get full artist credit for the recording.
+    /// </summary>
+    /// <returns>Artist credit.</returns>
+    public string GetCreditString()
+    {
+        var credit = new StringBuilder();
+        foreach (var artistCredit in ArtistCredit)
+        {
+            credit.Append(artistCredit.Name);
+            credit.Append(artistCredit.JoinPhrase);
+        }
+
+        return credit.ToString();
+    }
+}
+
+/// <summary>
+/// Artist credit model.
+/// </summary>
+public class ArtistCredit
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArtistCredit"/> class.
+    /// </summary>
+    public ArtistCredit()
+    {
+        JoinPhrase = string.Empty;
+        Name = string.Empty;
+    }
+
+    /// <summary>
+    /// Gets or sets join phrase.
+    /// </summary>
+    [JsonPropertyName("Joinphrase")]
+    public string JoinPhrase { get; set; }
+
+    /// <summary>
+    /// Gets or sets name.
+    /// </summary>
+    public string Name { get; set; }
 }
