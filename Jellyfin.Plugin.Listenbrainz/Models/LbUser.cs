d3df1037f57ab0521823322c0a8250cb1cc9b3f8
Dominik Krivohlavek <github@xkrivo.net>
2022-08-06 22:04:54 +0200

    Move user options to separate file

--- Jellyfin.Plugin.Listenbrainz/Models/LbUser.cs
+++ Jellyfin.Plugin.Listenbrainz/Models/LbUser.cs
@@ -56,20 +56,4 @@ namespace Jellyfin.Plugin.Listenbrainz.Models
             return (true, string.Empty);
         }
     }
-
-    /// <summary>
-    /// Listenbrainz user options.
-    /// </summary>
-    public class UserOptions
-    {
-        /// <summary>
-        /// Gets or sets a value indicating whether submitting listens is enabled.
-        /// </summary>
-        public bool ListenSubmitEnabled { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether favorites syncing is enabled.
-        /// </summary>
-        public bool SyncFavoritesEnabled { get; set; }
-    }
 }
