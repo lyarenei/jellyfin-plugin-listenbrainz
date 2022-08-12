332c013a3433b2136d0b4d0371d7756b249cbb23
Dominik Krivohlavek <github@xkrivo.net>
2022-08-12 08:07:09 +0200

    Add tests for kebabcase extension

--- Jellyfin.Plugin.Listenbrainz.Tests/ExtensionsTest.cs
+++ Jellyfin.Plugin.Listenbrainz.Tests/ExtensionsTest.cs
@@ -14,4 +14,14 @@ public class ExtensionsTest
     {
         Assert.Equal(expected, input.ToSnakeCase());
     }
+
+    [Theory]
+    [InlineData("kebabCase", "kebab-case")]
+    [InlineData("Kebabcase", "kebabcase")]
+    [InlineData("KebAbcAse", "keb-abc-ase")]
+    [InlineData("KebabcasE", "kebabcas-e")]
+    public void Extensions_ConvertToKebabCase(string input, string expected)
+    {
+        Assert.Equal(expected, input.ToKebabCase());
+    }
 }
