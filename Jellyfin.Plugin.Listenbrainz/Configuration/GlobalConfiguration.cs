3ec7b3c483846d404d4579ebe5a703fe07daf72f
Dominik Krivohlavek <github@xkrivo.net>
2022-12-05 23:49:59 +0100

    Fix docstrings

--- Jellyfin.Plugin.Listenbrainz/Configuration/GlobalConfiguration.cs
+++ Jellyfin.Plugin.Listenbrainz/Configuration/GlobalConfiguration.cs
@@ -8,19 +8,19 @@ namespace Jellyfin.Plugin.Listenbrainz.Configuration
     public class GlobalConfiguration
     {
         /// <summary>
-        /// Listenbrainz API base URL.
+        /// Gets or sets Listenbrainz API base URL.
         /// </summary>
         [SuppressMessage("Usage", "CA2227", Justification = "Needed for deserialization.")]
         public string? ListenbrainzBaseUrl { get; set; }
 
         /// <summary>
-        /// Musicbrainz API base URL.
+        /// Gets or sets Musicbrainz API base URL.
         /// </summary>
         [SuppressMessage("Usage", "CA2227", Justification = "Needed for deserialization.")]
         public string? MusicbrainzBaseUrl { get; set; }
 
         /// <summary>
-        /// Musicbrainz integration is enabled.
+        /// Gets or sets flag indicating a Musicbrainz integration is enabled.
         /// </summary>
         [SuppressMessage("Usage", "CA2227", Justification = "Needed for deserialization.")]
         public bool MusicbrainzEnabled { get; set; }
