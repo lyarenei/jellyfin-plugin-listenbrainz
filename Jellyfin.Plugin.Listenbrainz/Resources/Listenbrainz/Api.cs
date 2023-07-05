e775fe83a92d848c178446dd9f85377a3217b30e
Dominik Krivohlavek <github@xkrivo.net>
2023-07-05 14:23:57 +0200

    Add listens count limit

--- Jellyfin.Plugin.Listenbrainz/Resources/Listenbrainz/Api.cs
+++ Jellyfin.Plugin.Listenbrainz/Resources/Listenbrainz/Api.cs
@@ -14,5 +14,11 @@ namespace Jellyfin.Plugin.Listenbrainz.Resources.Listenbrainz
         /// API base URL.
         /// </summary>
         public const string BaseUrl = "https://api.listenbrainz.org";
+
+        /// <summary>
+        /// Maximum listens to send in a request.
+        /// API docs states this limit is set to 1000, we will be a bit conservative.
+        /// </summary>
+        public const int MaxListensPerRequest = 100;
     }
 }
