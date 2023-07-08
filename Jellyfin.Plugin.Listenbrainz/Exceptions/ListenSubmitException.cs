60aa3c683b68e6a6a50f83fe5bcf06616acabfaa
Dominik Krivohlavek <github@xkrivo.net>
2023-07-08 22:52:18 +0200

    Update exception implementation

--- Jellyfin.Plugin.Listenbrainz/Exceptions/ListenSubmitException.cs
+++ Jellyfin.Plugin.Listenbrainz/Exceptions/ListenSubmitException.cs
@@ -10,8 +10,24 @@ public class ListenSubmitException : Exception
     /// <summary>
     /// Initializes a new instance of the <see cref="ListenSubmitException"/> class.
     /// </summary>
-    /// <param name="msg">Exception message.</param>
-    public ListenSubmitException(string msg = "") : base(msg)
+    public ListenSubmitException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ListenSubmitException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public ListenSubmitException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ListenSubmitException"/> class.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="inner">Inner exception.</param>
+    public ListenSubmitException(string message, Exception inner) : base(message, inner)
     {
     }
 }
