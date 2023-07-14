78758552f010c70602627a78da664f640bb2f9f2
Dominik Krivohlavek <github@xkrivo.net>
2023-07-14 16:19:18 +0200

    Build Listen from Audio

--- src/ListenBrainzPlugin/Extensions/AudioExtensions.cs
+++ src/ListenBrainzPlugin/Extensions/AudioExtensions.cs
@@ -1,3 +1,4 @@
+using ListenBrainzPlugin.ListenBrainzApi.Models;
 using MediaBrowser.Controller.Entities.Audio;
 
 namespace ListenBrainzPlugin.Extensions;
@@ -17,5 +18,41 @@ public static class AudioExtensions
         if (!artistNames.Any()) throw new ArgumentException("Item has no valid artists");
 
         if (string.IsNullOrWhiteSpace(item.Name)) throw new ArgumentException("Item name is empty");
+    }
+
+    /// <summary>
+    /// Transforms an <see cref="Audio"/> item to a <see cref="Listen"/>.
+    /// </summary>
+    /// <param name="item">Item to transform.</param>
+    /// <param name="timestamp">Timestamp of the listen.</param>
+    /// <returns>Listen instance with data from the item.</returns>
+    public static Listen AsListen(this Audio item, long? timestamp = null)
+    {
+        return new Listen
+        {
+            ListenedAt = timestamp,
+            TrackMetadata = new TrackMetadata
+            {
+                ArtistName = string.Join(", ", item.Artists.TakeWhile(name => !string.IsNullOrEmpty(name))),
+                ReleaseName = item.Album,
+                TrackName = item.Name,
+                AdditionalInfo = new AdditionalInfo
+                {
+                    MediaPlayer = "Jellyfin",
+                    MediaPlayerVersion = null,
+                    SubmissionClient = "ListenBrainz plugin for Jellyfin",
+                    SubmissionClientVersion = Plugin.Version,
+                    ReleaseMbid = item.ProviderIds.GetValueOrDefault("MusicBrainzAlbum"),
+                    ArtistMbids = item.ProviderIds.GetValueOrDefault("MusicBrainzArtist")?.Split(';'),
+                    ReleaseGroupMbid = item.ProviderIds.GetValueOrDefault("MusicBrainzReleaseGroup"),
+                    RecordingMbid = null,
+                    TrackMbid = item.ProviderIds.GetValueOrDefault("MusicBrainzTrack"),
+                    WorkMbids = null,
+                    TrackNumber = item.IndexNumber,
+                    Tags = item.Tags,
+                    DurationMs = (item.RunTimeTicks / TimeSpan.TicksPerSecond) * 1000,
+                }
+            }
+        };
     }
 }
