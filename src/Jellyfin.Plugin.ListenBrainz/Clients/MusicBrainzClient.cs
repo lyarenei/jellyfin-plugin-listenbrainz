e3313693795043471d7abe14e5f6656a4c4cbc89
Dominik Krivohlavek <github@xkrivo.net>
2023-07-19 17:45:07 +0200

    Update MusicBrainz client to specify configured api url

--- src/Jellyfin.Plugin.ListenBrainz/Clients/MusicBrainzClient.cs
+++ src/Jellyfin.Plugin.ListenBrainz/Clients/MusicBrainzClient.cs
@@ -34,7 +34,8 @@ public class MusicBrainzClient : IMetadataClient
         var trackMbid = item.GetTrackMbid();
         if (trackMbid is null) throw new ArgumentException("Audio item does not have a track MBID");
 
-        var request = new RecordingRequest(trackMbid);
+        var config = Plugin.GetConfiguration();
+        var request = new RecordingRequest(trackMbid) { BaseUrl = config.MusicBrainzApiUrl };
         var response = await _apiClient.GetRecording(request, CancellationToken.None);
         if (response is null) throw new MetadataException("No response received");
         if (!response.Recordings.Any()) throw new MetadataException("No metadata in response");
