753789004a79a10ce4b6d60fb9caf26b40da726d
Dominik Krivohlavek <github@xkrivo.net>
2023-08-05 20:26:23 +0200

    Extract method for getting all libraries

--- src/Jellyfin.Plugin.ListenBrainz/Extensions/LibraryManagerExtensions.cs
+++ src/Jellyfin.Plugin.ListenBrainz/Extensions/LibraryManagerExtensions.cs
@@ -9,15 +9,26 @@ namespace Jellyfin.Plugin.ListenBrainz.Extensions;
 public static class LibraryManagerExtensions
 {
     /// <summary>
+    /// Get all libraries.
+    /// </summary>
+    /// <param name="libraryManager">Jellyfin library manager.</param>
+    /// <returns>All music libraries on server.</returns>
+    public static IEnumerable<Folder> GetLibraries(this ILibraryManager libraryManager)
+    {
+        return libraryManager
+            .GetUserRootFolder()
+            .Children
+            .Cast<Folder>();
+    }
+
+    /// <summary>
     /// Get all music libraries.
     /// </summary>
     /// <param name="libraryManager">Jellyfin library manager.</param>
     /// <returns>All music libraries on server.</returns>
     public static IEnumerable<Folder> GetMusicLibraries(this ILibraryManager libraryManager)
     {
-        return libraryManager
-            .GetUserRootFolder()
-            .Children
+        return GetLibraries(libraryManager)
             .Cast<CollectionFolder>()
             .Where(f => f.CollectionType == "music");
     }
