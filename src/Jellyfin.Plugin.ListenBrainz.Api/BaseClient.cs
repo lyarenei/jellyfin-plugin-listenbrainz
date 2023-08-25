3586e5e0569746102d9bf7ef4a732047cacb58de
Dominik Krivohlavek <github@xkrivo.net>
2023-08-25 09:59:43 +0200

    Make method static

--- src/Jellyfin.Plugin.ListenBrainz.Api/BaseClient.cs
+++ src/Jellyfin.Plugin.ListenBrainz.Api/BaseClient.cs
@@ -85,7 +85,7 @@ public class BaseClient : HttpClient
         using (requestMessage) return await DoRequest<TResponse>(requestMessage, cancellationToken);
     }
 
-    private Uri BuildRequestUri(string baseUrl, string endpoint) => new($"{baseUrl}/{Resources.General.Version}/{endpoint}");
+    private static Uri BuildRequestUri(string baseUrl, string endpoint) => new($"{baseUrl}/{Resources.General.Version}/{endpoint}");
 
     private async Task<TResponse?> DoRequest<TResponse>(HttpRequestMessage requestMessage, CancellationToken cancellationToken)
         where TResponse : IListenBrainzResponse
