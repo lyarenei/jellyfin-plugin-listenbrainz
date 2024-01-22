15011a4365739a5130796be0bef3c9d816a7cbbb
Dominik Krivohlavek <github@xkrivo.net>
2024-01-22 14:11:58 +0100

    Include ISRC in additional info

--- src/Jellyfin.Plugin.ListenBrainz/Extensions/AudioExtensions.cs
+++ src/Jellyfin.Plugin.ListenBrainz/Extensions/AudioExtensions.cs
@@ -53,6 +53,7 @@ public static class AudioExtensions
                     TrackMbid = item.ProviderIds.GetValueOrDefault("MusicBrainzTrack"),
                     WorkMbids = null,
                     TrackNumber = item.IndexNumber,
+                    Isrc = itemMetadata?.Isrcs.FirstOrDefault(),
                     Tags = item.Tags,
                     DurationMs = (item.RunTimeTicks / TimeSpan.TicksPerSecond) * 1000
                 }
