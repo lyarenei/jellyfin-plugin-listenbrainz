8a36280361b19283848754d87b195a0b1db40761
Dominik Krivohlavek <github@xkrivo.net>
2024-02-05 00:18:37 +0100

    Rename parameter

--- src/Jellyfin.Plugin.ListenBrainz/Clients/MetadataClient.cs
+++ src/Jellyfin.Plugin.ListenBrainz/Clients/MetadataClient.cs
@@ -21,10 +21,10 @@ public sealed class MetadataClient : IMetadataClient, IDisposable
     /// </summary>
     /// <param name="clientName">Name of the client application..</param>
     /// <param name="version">Version of the client application.</param>
-    /// <param name="sourceUrl">Contact URL for the maintainer of the application.</param>
-    public MetadataClient(string clientName, string version, string sourceUrl)
+    /// <param name="contactUrl">Contact URL for the maintainer of the application.</param>
+    public MetadataClient(string clientName, string version, string contactUrl)
     {
-        _query = new Query(clientName, version, sourceUrl);
+        _query = new Query(clientName, version, contactUrl);
         _isDisposed = false;
     }
 
