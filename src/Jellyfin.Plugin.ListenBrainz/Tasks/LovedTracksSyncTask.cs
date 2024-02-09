402dc8a5d2d45ad5c39c424cb33f4d3fad0f04e9
Dominik Krivohlavek <github@xkrivo.net>
2024-02-10 00:15:01 +0100

    Add task skeleton

---
+++ src/Jellyfin.Plugin.ListenBrainz/Tasks/LovedTracksSyncTask.cs
@@ -0,0 +1,53 @@
+using MediaBrowser.Model.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Jellyfin.Plugin.ListenBrainz.Tasks;
+
+/// <summary>
+/// Scheduled Jellyfin task for syncing loved tracks from ListenBrainz to Jellyfin.
+/// </summary>
+public class LovedTracksSyncTask : IScheduledTask
+{
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LovedTracksSyncTask"/> class.
+    /// </summary>
+    /// <param name="loggerFactory">Logger factory.</param>
+    public LovedTracksSyncTask(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger($"{Plugin.LoggerCategory}.FavoriteSyncTask");
+    }
+
+    /// <inheritdoc />
+    public string Name => "Synchronize loved tracks";
+
+    /// <inheritdoc />
+    public string Key => "SyncLovedTracks";
+
+    /// <inheritdoc />
+    public string Description => "Synchronize loved tracks from ListenBrainz to Jellyfin";
+
+    /// <inheritdoc />
+    public string Category => "ListenBrainz";
+
+    /// <inheritdoc />
+    public IEnumerable<TaskTriggerInfo> GetDefaultTriggers() => Array.Empty<TaskTriggerInfo>();
+
+    /// <inheritdoc />
+    public async Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+    {
+        // TODO fail if musicbrainz integration is disabled
+        var conf = Plugin.GetConfiguration();
+        foreach (var userConfig in conf.UserConfigs)
+        {
+            if (!userConfig.IsFavoritesSyncEnabled)
+            {
+                _logger.LogDebug("User has not favorite syncing enabled, skipping");
+                continue;
+            }
+
+            // TODO: handle user sync
+        }
+    }
+}
