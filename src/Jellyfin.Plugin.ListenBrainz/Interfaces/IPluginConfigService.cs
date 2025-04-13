07ffa2089f977d6ee6e1853f593810c1606de3f3
Dominik Krivohlavek <github@xkrivo.net>
2025-04-13 15:21:20 +0200

    Add a plugin config service

---
+++ src/Jellyfin.Plugin.ListenBrainz/Interfaces/IPluginConfigService.cs
@@ -0,0 +1,16 @@
+using Jellyfin.Plugin.ListenBrainz.Configuration;
+
+namespace Jellyfin.Plugin.ListenBrainz.Interfaces;
+
+/// <summary>
+/// A service for accessing plugin configuration.
+/// </summary>
+public interface IPluginConfigService
+{
+    /// <summary>
+    /// Get a configuration for a specified Jellyfin user ID.
+    /// </summary>
+    /// <param name="jellyfinUserId">ID of the Jellyfin user.</param>
+    /// <returns>User configuration. Null if it does not exist.</returns>
+    public UserConfig? GetUserConfig(Guid jellyfinUserId);
+}
