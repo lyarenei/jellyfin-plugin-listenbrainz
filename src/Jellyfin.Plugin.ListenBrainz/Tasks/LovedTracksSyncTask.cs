0e2c578fd4c25a2ea9ef405f28d185cddf9dba45
Dominik Krivohlavek <github@xkrivo.net>
2025-06-18 22:18:30 +0200

    Use recording mbid in sync task if available

--- src/Jellyfin.Plugin.ListenBrainz/Tasks/LovedTracksSyncTask.cs
+++ src/Jellyfin.Plugin.ListenBrainz/Tasks/LovedTracksSyncTask.cs
@@ -158,7 +158,7 @@ public class LovedTracksSyncTask : IScheduledTask
         var items = _libraryManager
             .GetItemList(q, allowedLibraries.ToList())
             .Where(i => !_userDataManager.GetUserData(user, i).IsFavorite)
-            .Where(i => i.ProviderIds.GetValueOrDefault("MusicBrainzTrack") is not null)
+            .Where(i => i.GetRecordingMbid() is not null || i.ProviderIds.GetValueOrDefault("MusicBrainzTrack") is not null)
             .ToList();
 
         foreach (var item in items)
@@ -168,7 +168,7 @@ public class LovedTracksSyncTask : IScheduledTask
 
             try
             {
-                recordingMbid = _musicBrainzClient.GetAudioItemMetadata(item).RecordingMbid;
+                recordingMbid = item.GetRecordingMbid() ?? _musicBrainzClient.GetAudioItemMetadata(item).RecordingMbid;
             }
             catch (Exception e)
             {
