c6237ecf97333802d6042cd46ba71c165142a454
Dominik Krivohlavek <github@xkrivo.net>
2025-12-23 22:43:33 +0100

    Get LB API client util

--- src/Jellyfin.Plugin.ListenBrainz/Clients/Utils.cs
+++ src/Jellyfin.Plugin.ListenBrainz/Clients/Utils.cs
@@ -1,4 +1,5 @@
 using Jellyfin.Plugin.ListenBrainz.Api;
+using Jellyfin.Plugin.ListenBrainz.Api.Interfaces;
 using Jellyfin.Plugin.ListenBrainz.Common.Extensions;
 using Jellyfin.Plugin.ListenBrainz.Interfaces;
 using Jellyfin.Plugin.ListenBrainz.MusicBrainzApi;
@@ -28,6 +29,21 @@ public static class Utils
         var apiClient = new ListenBrainzApiClient(baseClient, logger);
         var pluginConfig = new DefaultPluginConfigService();
         return new ListenBrainzClient(logger, apiClient, pluginConfig);
+    }
+
+    /// <summary>
+    /// Get a ListenBrainz API client.
+    /// </summary>
+    /// <param name="logger">Logger instance.</param>
+    /// <param name="clientFactory">HTTP client factory.</param>
+    /// <returns>ListenBrainz client.</returns>
+    public static IListenBrainzApiClient GetListenBrainzApiClient(
+        ILogger logger,
+        IHttpClientFactory clientFactory)
+    {
+        var httpClient = new UnderlyingClient(clientFactory, logger, null);
+        var baseClient = new BaseApiClient(new HttpClientWrapper(httpClient), logger, null);
+        return new ListenBrainzApiClient(baseClient, logger);
     }
 
     /// <summary>
