a01f03909d1bb2035a59b3c6843554f6c2ba6ed5
Dominik Krivohlavek <github@xkrivo.net>
2026-02-14 21:08:44 +0100

    Simplify docstrings

--- src/Jellyfin.Plugin.ListenBrainz/Pages/Configuration/constants.ts
+++ src/Jellyfin.Plugin.ListenBrainz/Pages/Configuration/constants.ts
@@ -1,11 +1,7 @@
-/**
- * ID of the plugin. Must be the same as the ID in Plugin.cs file.
- */
+// ID of the plugin. Must be the same as the ID in Plugin.cs file.
 export const pluginUUID = "59B20823-AAFE-454C-A393-17427F518631";
 
-/**
- * Default values for the plugin user configuration.
- */
+// Default values for the plugin user configuration.
 export const userDefaults: PluginUserConfig = {
     ApiToken: "",
     IsFavoritesSyncEnabled: false,
