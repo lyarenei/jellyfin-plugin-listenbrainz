9d05dbf4ae8f8ae70d70ed625b11913008c99712
Dominik Krivohlavek <github@xkrivo.net>
2026-02-14 17:16:13 +0100

    Fix user config fields

--- src/Jellyfin.Plugin.ListenBrainz/Configuration/configurationPage.ts
+++ src/Jellyfin.Plugin.ListenBrainz/Configuration/configurationPage.ts
@@ -7,15 +7,15 @@ const pluginUUID = "59B20823-AAFE-454C-A393-17427F518631";
  * Default values for the plugin user configuration.
  * The keys must match the keys of the user configuration model defined in UserConfig.cs file.
  */
-const userDefaults = {
+const userDefaults: PluginUserConfig = {
     ApiToken: "",
-    IsAllPlaylistsSyncEnabled: false,
     IsFavoritesSyncEnabled: false,
     IsListenSubmitEnabled: false,
     IsPlaylistsSyncEnabled: false,
     IsStrictModeEnabled: false,
-    IsUserBackupEnabled: false,
+    IsBackupEnabled: false,
     JellyfinUserId: "",
+    UserName: "",
 };
 
 /**
