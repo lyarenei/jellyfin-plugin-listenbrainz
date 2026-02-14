2d7d624bc6f141eb2595285c920189e700854cea
Dominik Krivohlavek <github@xkrivo.net>
2026-02-14 13:26:57 +0100

    Integrate compiled js to plugin

--- src/Jellyfin.Plugin.ListenBrainz/Plugin.cs
+++ src/Jellyfin.Plugin.ListenBrainz/Plugin.cs
@@ -151,9 +151,12 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages, IDisposable
             new PluginPageInfo
             {
                 Name = Name,
-                EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configurationPage.html",
-                EnableInMainMenu = false,
-                MenuIcon = "music_note",
+                EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configurationPage.html", GetType().Namespace),
+            },
+            new PluginPageInfo
+            {
+                Name = $"{Name}.js",
+                EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configurationPage.js", GetType().Namespace),
             },
         ];
     }
