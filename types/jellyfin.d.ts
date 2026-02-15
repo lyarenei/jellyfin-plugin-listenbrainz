eafa41d1c2b72a34f689d223fbdcbb93224627cd
Dominik Krivohlavek <github@xkrivo.net>
2026-02-15 11:10:00 +0100

    Move some types

--- types/jellyfin.d.ts
+++ types/jellyfin.d.ts
@@ -26,28 +26,5 @@ interface JellyfinUser {
     Name: string;
 }
 
-interface PluginConfiguration {
-    UserConfigs: PluginUserConfig[];
-}
-
-// Plugin user configuration.
-// The keys must match the fields of UserConfig class.
-interface PluginUserConfig {
-    ApiToken: string;
-    IsBackupEnabled: boolean;
-    IsFavoritesSyncEnabled: boolean;
-    IsListenSubmitEnabled: boolean;
-    IsPlaylistsSyncEnabled: boolean;
-    IsStrictModeEnabled: boolean;
-    JellyfinUserId: string;
-    UserName: string;
-}
-
-interface TokenValidationResult {
-    IsValid: boolean;
-    Reason: string;
-    UserName: string;
-}
-
 declare const ApiClient: JellyfinApiClient;
 declare const Dashboard: JellyfinDashboard;
