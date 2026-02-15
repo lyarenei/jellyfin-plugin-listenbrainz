7391cca7b6537c7d5316ebee71c643a9d339a4bb
Dominik Krivohlavek <github@xkrivo.net>
2026-02-15 16:35:50 +0100

    Wire up reset API URL buttons logic

--- src/Jellyfin.Plugin.ListenBrainz/Pages/Configuration/eventHooks/index.ts
+++ src/Jellyfin.Plugin.ListenBrainz/Pages/Configuration/eventHooks/index.ts
@@ -1,9 +1,14 @@
 import { registerUserDropdownChangeHook } from "./userDropdown";
 import { registerApiTokenCheckButtonHook } from "./apiToken";
 import { registerSubmitButtonHook } from "./submit";
+import { registerResetListenBrainzApiUrlButtonHook, registerResetMusicBrainzApiUrlButtonHook } from "./button";
 
 export default function registerEventHooks(view: HTMLElement) {
     registerUserDropdownChangeHook(view);
     registerApiTokenCheckButtonHook(view);
+
+    registerResetListenBrainzApiUrlButtonHook(view);
+    registerResetMusicBrainzApiUrlButtonHook(view);
+
     registerSubmitButtonHook(view);
 }
