2b9bf9652660b362e158b8adc8f198e586ef4ea3
Dominik Krivohlavek <github@xkrivo.net>
2026-06-26 08:10:04 +0200

    Finish up the task implementation

--- src/Jellyfin.Plugin.ListenBrainz/Tasks/SyncWeeklyPlaylistsTask.cs
+++ src/Jellyfin.Plugin.ListenBrainz/Tasks/SyncWeeklyPlaylistsTask.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using Jellyfin.Data.Enums;
 using Jellyfin.Database.Implementations.Entities;
 using Jellyfin.Plugin.ListenBrainz.Api.Resources;
 using Jellyfin.Plugin.ListenBrainz.Common.Extensions;
@@ -8,8 +10,10 @@ using Jellyfin.Plugin.ListenBrainz.Interfaces;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Playlists;
+using MediaBrowser.Model.Playlists;
 using MediaBrowser.Model.Tasks;
 using Microsoft.Extensions.Logging;
+using JellyfinPlaylist = MediaBrowser.Controller.Playlists.Playlist;
 using Playlist = Jellyfin.Plugin.ListenBrainz.Api.Models.Playlist;
 using Utils = Jellyfin.Plugin.ListenBrainz.Common.Utils;
 
@@ -20,11 +24,11 @@ namespace Jellyfin.Plugin.ListenBrainz.Tasks;
 /// </summary>
 public class SyncWeeklyPlaylistsTask : IScheduledTask
 {
+    private const string PlaylistTag = "ListenBrainz";
     private const int RotationPlaylistCount = 2;
 
     private readonly ILogger _logger;
     private readonly IListenBrainzService _listenBrainz;
-    private readonly IMetadataProviderService _metadataProvider;
     private readonly ILibraryManager _libraryManager;
     private readonly IUserManager _userManager;
     private readonly IPlaylistManager _playlistManager;
@@ -42,7 +46,6 @@ public class SyncWeeklyPlaylistsTask : IScheduledTask
     /// <param name="userManager">User manager.</param>
     /// <param name="playlistManager">Playlist manager.</param>
     /// <param name="listenBrainz">ListenBrainz service.</param>
-    /// <param name="metadataProvider">Metadata provider service.</param>
     /// <param name="configService">Plugin configuration service.</param>
     /// <param name="stateService">Playlist sync state service.</param>
     /// <param name="trackMatcher">Playlist track matcher.</param>
@@ -52,14 +55,12 @@ public class SyncWeeklyPlaylistsTask : IScheduledTask
         IUserManager userManager,
         IPlaylistManager playlistManager,
         IListenBrainzService listenBrainz,
-        IMetadataProviderService metadataProvider,
         IPluginConfigService configService,
         IPlaylistSyncStateService stateService,
         IPlaylistTrackMatcher trackMatcher)
     {
         _logger = loggerFactory.CreateLogger($"{Plugin.LoggerCategory}.SyncWeeklyPlaylistsTask");
         _listenBrainz = listenBrainz;
-        _metadataProvider = metadataProvider;
         _libraryManager = libraryManager;
         _userManager = userManager;
         _playlistManager = playlistManager;
@@ -252,9 +253,139 @@ public class SyncWeeklyPlaylistsTask : IScheduledTask
             tracks.Count,
             playlist.Title);
 
-        // todo: handle sync state
+        var mapping = state.FindMapping(user.Id, playlist.PlaylistId);
+        var existingPlaylist =
+            GetMappedPlaylist(user, state, mapping) ?? FindTaggedPlaylistByName(user, playlist.Title);
+
+        Guid jellyfinPlaylistId;
+        if (existingPlaylist is null)
+        {
+            jellyfinPlaylistId = await CreatePlaylist(user, playlist.Title, matchedTracks, cancellationToken);
+        }
+        else
+        {
+            jellyfinPlaylistId = existingPlaylist.Id;
+            await UpdatePlaylist(user, existingPlaylist, matchedTracks, cancellationToken);
+        }
+
+        state.Upsert(
+            user.Id,
+            playlist.PlaylistId,
+            jellyfinPlaylistId,
+            playlist.Title,
+            playlist.CreatedAt,
+            playlistType.ToString());
+
+        _logger.LogInformation(
+            "Successfully synced weekly playlist {Name} with {Count} tracks",
+            playlist.Title,
+            matchedTracks.Count);
     }
 
+    private async Task<Guid> CreatePlaylist(
+        User user,
+        string playlistName,
+        List<BaseItem> tracks,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogDebug("Creating weekly playlist {Name} with {Count} items", playlistName, tracks.Count);
+        var createdPlaylist = await _playlistManager.CreatePlaylist(new PlaylistCreationRequest
+        {
+            Name = playlistName,
+            UserId = user.Id,
+            ItemIdList = tracks.Select(i => i.Id).ToArray(),
+            MediaType = MediaType.Audio,
+        });
+
+        if (!Guid.TryParse(createdPlaylist.Id, out var playlistId))
+        {
+            throw new PluginException($"Created playlist ID '{createdPlaylist.Id}' is invalid");
+        }
+
+        await TagPlaylist(playlistId, user.Id, cancellationToken);
+        return playlistId;
+    }
+
+    private async Task UpdatePlaylist(
+        User user,
+        JellyfinPlaylist playlist,
+        List<BaseItem> tracks,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogDebug("Updating weekly playlist {Name} with {Count} items", playlist.Name, tracks.Count);
+
+        var entryIds = playlist
+            .GetLinkedChildrenInfos()
+            .Select(i => i.Item1.ItemId?.ToString("N", CultureInfo.InvariantCulture))
+            .Where(id => !string.IsNullOrWhiteSpace(id))
+            .ToArray();
+        if (entryIds.Length > 0)
+        {
+            await _playlistManager.RemoveItemFromPlaylistAsync(
+                playlist.Id.ToString("N", CultureInfo.InvariantCulture),
+                entryIds!);
+        }
+
+        await _playlistManager.AddItemToPlaylistAsync(
+            playlist.Id,
+            tracks.Select(i => i.Id).ToArray(),
+            user.Id);
+        await TagPlaylist(playlist.Id, user.Id, cancellationToken);
+    }
+
+    private async Task TagPlaylist(Guid playlistId, Guid userId, CancellationToken cancellationToken)
+    {
+        var playlist = _playlistManager.GetPlaylistForUser(playlistId, userId);
+        if (playlist is null)
+        {
+            _logger.LogWarning("Could not tag playlist {PlaylistId}: playlist was not found", playlistId);
+            return;
+        }
+
+        if (HasListenBrainzTag(playlist))
+        {
+            return;
+        }
+
+        playlist.Tags = [..playlist.Tags, PlaylistTag];
+        await playlist.UpdateToRepositoryAsync(ItemUpdateType.MetadataEdit, cancellationToken);
+    }
+
+    private JellyfinPlaylist? GetMappedPlaylist(User user, PlaylistSyncState state, PlaylistMapping? mapping)
+    {
+        if (mapping is null)
+        {
+            return null;
+        }
+
+        var playlist = _playlistManager.GetPlaylistForUser(mapping.JellyfinPlaylistId, user.Id);
+        if (playlist is not null)
+        {
+            return playlist;
+        }
+
+        _logger.LogInformation(
+            "Mapped Jellyfin playlist {PlaylistId} for ListenBrainz playlist {ListenBrainzPlaylistId} no longer exists",
+            mapping.JellyfinPlaylistId,
+            mapping.ListenBrainzPlaylistId);
+        state.Mappings.Remove(mapping);
+        return null;
+    }
+
+    private JellyfinPlaylist? FindTaggedPlaylistByName(User user, string playlistName)
+    {
+        var playlistQuery = new InternalItemsQuery
+        {
+            IncludeItemTypes = [BaseItemKind.Playlist],
+            Name = playlistName,
+            User = user,
+        };
+
+        return _libraryManager
+            .GetItemList(playlistQuery)
+            .OfType<JellyfinPlaylist>()
+            .FirstOrDefault(HasListenBrainzTag);
+    }
 
     private void PruneOutOfRotationPlaylists(
         User user,
@@ -367,6 +498,11 @@ public class SyncWeeklyPlaylistsTask : IScheduledTask
             WeeklyPlaylistType.Exploration => userConfig.IsWeeklyExplorationSyncEnabled,
             _ => false,
         };
+    }
+
+    private static bool HasListenBrainzTag(BaseItem playlist)
+    {
+        return playlist.Tags.Any(tag => tag.Equals(PlaylistTag, StringComparison.OrdinalIgnoreCase));
     }
 
     private void ResetProgress(int userCount)
